b2565c839edfb7fbb7977e9ff749299dfb5a6c7a
Nely Rambelomanana <nely.rambelomanana@clirisgroup.com>
2023-04-28 18:36:39 +0200

    update root

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from db import items, stores
+from flask_smorest import abort
 import uuid
 
 app = Flask(__name__)
@@ -16,20 +17,42 @@ def hello():
 def create_store():
     """Create a store"""
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. ensure name is included in json payload.",
+        )
+    for store in stores.values():
+        if store["name"] == store_data["name"]:
+            abort(400, message="Store already exist")
     store_id = uuid.uuid4().hex
     new_store = {**store_data, "id": store_id}
     stores[store_id] = new_store
     return new_store, 201
 
 
-@app.post("/store/item")
-def create_item(name):
+@app.post("/item")
+def create_item():
     """create an item"""
     item_data = request.get_json()
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. ensure price, store_id, and name are included in json payload.",
+        )
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        # return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
+    for item in items.values():
+        if item_data["name"] == item["name"]:
+            abort(400, message="Item already exist")
     item_id = uuid.uuid4().hex
     new_item = {**item_data, "id": item_id}
+    items[item_id] = new_item
     return new_item, 201
 
 
@@ -40,7 +63,7 @@ def get_stores():
 
 
 @app.get("/item")
-def get_stores():
+def get_items():
     """Get all titems in db"""
     return {"items": list(items.values())}
 
@@ -51,16 +74,46 @@ def get_store(store_id):
     try:
         return stores[store_id], 200
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
 
 
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
-    """get item in specific store"""
+    """get item"""
     try:
         return items[item_id], 200
     except KeyError:
-        return {"message": "item not found"}, 404
+        abort(404, message="Item not found")
+
+
+@app.delete(("/item/<string:item_id>"))
+def delete_item(item_id):
+    """delete item"""
+    try:
+        del items[item_id]
+        return {"message": "Item deleted"}, 200
+    except KeyError:
+        abort(404, message="Item not found")
+
+
+@app.put(("/item/<string:item_id>"))
+def update_item(item_id):
+    """update item"""
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(
+            400,
+            message="Bad request. ensure price and name are included in json payload.",
+        )
+    try:
+        item = items[item_id]
+        # item["name"] = item_data["name"]
+        # item["price"] = item_data["price"]
+        item |= item_data
+        return item, 200
+
+    except KeyError:
+        abort(404, message="Item not found")
 
 
 # if __name__ == "__main__":
--- test_store_api.py
+++ test_store_api.py
@@ -15,21 +15,71 @@ def test_get_stores():
     pass
 
 
-def test_get_one_specific_store():
-    store_name = "my store"
-    response = requests.get(f"{ENDPOINT}/store/{store_name}")
+def test_get_items():
+    response = requests.get(f"{ENDPOINT}/item")
     assert response.status_code == 200
     pass
 
 
 def test_create_store():
-    payload = {"name": "test", "items": [{"name": "Table", "price": 25.99}]}
+    payload = {"name": "store 1"}
     response = requests.post(f"{ENDPOINT}/store", json=payload)
     assert response.status_code == 201
 
 
-def test_create_item_in_a_store():
-    store_name = "test"
-    payload = {"name": "TV", "price": 100}
-    response = requests.post(f"{ENDPOINT}/store/{store_name}/item", json=payload)
+def test_create_item():
+    payload = {"name": "store 2"}
+    response = requests.post(f"{ENDPOINT}/store", json=payload)
     assert response.status_code == 201
+
+
+def test_create_item():
+    payload = {"name": "store 2"}
+    response = requests.post(f"{ENDPOINT}/store", json=payload)
+    assert response.status_code == 201
+    store_data = response.json()
+    store_id = store_data["id"]
+    payload = {"store_id": store_id, "price": 105, "name": "Tv"}
+    response = requests.post(f"{ENDPOINT}/item", json=payload)
+    assert response.status_code == 201
+
+
+def test_delete_item():
+    store_payload = {"name": "store 3 to be deleted"}
+    store_response = requests.post(f"{ENDPOINT}/store", json=store_payload)
+    assert store_response.status_code == 201
+    store_data = store_response.json()
+
+    store_id = store_data["id"]
+    item_payload = {"store_id": store_id, "price": 10, "name": "item to be deleted"}
+    item_response = requests.post(f"{ENDPOINT}/item", json=item_payload)
+    assert item_response.status_code == 201
+    item_data = item_response.json()
+
+    item_id = item_data["id"]
+    delete_item_response = requests.delete(f"{ENDPOINT}/item/{item_id}")
+    assert delete_item_response.status_code == 200
+
+
+def test_update_item():
+    store_payload = {"name": "store 4 to be updated"}
+    store_response = requests.post(f"{ENDPOINT}/store", json=store_payload)
+    assert store_response.status_code == 201
+    store_data = store_response.json()
+
+    store_id = store_data["id"]
+    item_payload = {"store_id": store_id, "price": 10, "name": "item to be updated"}
+    item_response = requests.post(f"{ENDPOINT}/item", json=item_payload)
+    assert item_response.status_code == 201
+    item_data = item_response.json()
+
+    item_id = item_data["id"]
+    updated_item_payload = {"price": 15, "name": "item updated"}
+    updated_item_response = requests.put(
+        f"{ENDPOINT}/item/{item_id}", json=updated_item_payload
+    )
+    assert updated_item_response.status_code == 200
+
+
+if __name__ == "__main__":
+    test_update_item()
