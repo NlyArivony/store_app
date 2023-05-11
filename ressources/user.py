92cd16bfdf14c8607f38e713c4c17b55f93a8b4b
Nely Rambelomanana <nely.rambelomanana@clirisgroup.com>
2023-05-11 15:52:02 +0200

    implement mail function

--- ressources/user.py
+++ ressources/user.py
@@ -1,3 +1,6 @@
+import requests
+import os
+
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
@@ -11,28 +14,57 @@ from flask_jwt_extended import (
 
 from db import db
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import or_
 from models import UserModel
-from schemas import UserSchema
+from schemas import UserSchema, UserRegisterSchema
 
 from blocklist import BLOCKLIST
 
 blp = Blueprint("users", __name__, description="Operations on users")
 
 
+def send_simple_message(to, subject, body):
+    """Mailgun code to send mail"""
+    domain = os.getenv("MAILGUN_DOMAIN")
+    api_key = os.getenv("MAILGUN_API_KEY")
+    return requests.post(
+        f"https://api.mailgun.net/v3/{domain}/messages",
+        auth=("api", api_key),
+        data={
+            "from": f"store admin user <mailgun@{domain}>",
+            "to": [to],
+            "subject": subject,
+            "text": body,
+        },
+    )
+
+
 @blp.route("/register/")
 class UserRegister(MethodView):
-    @blp.arguments(UserSchema)
+    @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
-        """register a user"""
-        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
-            abort(409, message=" A user with that username already exists.")
+        """register a user and send email registered user"""
+        if UserModel.query.filter(
+            or_(
+                UserModel.username == user_data["username"],
+                UserModel.email == user_data["email"],
+            )
+        ).first():
+            abort(409, message=" A user with that username or email already exists.")
 
         user = UserModel(
             username=user_data["username"],
             password=pbkdf2_sha256.hash(user_data["password"]),
+            email=user_data["email"],
         )
         db.session.add(user)
         db.session.commit()
+
+        send_simple_message(
+            to=user.email,
+            subject="successfully signed up to store flask",
+            body=f"Hi {user.username}! You have successfully signed up to the stores REST API",
+        )
 
         return {"message": "User created successfully."}, 201
 
